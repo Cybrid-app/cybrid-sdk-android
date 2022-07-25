95e2bc76861c7db5a614455dbf2ca000813a8ea1
Erick Sanchez <erick.sanchez.p05@gmail.com>
2022-07-25 12:20:04 -0500

    Add: Setup DEmoApp

---
+++ app/src/main/java/app/cybrid/demoapp/ui/components/accounts/AccountsComponent.kt
@@ -0,0 +1,11 @@
+package app.cybrid.demoapp.ui.components.accounts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountsComponent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accounts_component)
+    }
+}
