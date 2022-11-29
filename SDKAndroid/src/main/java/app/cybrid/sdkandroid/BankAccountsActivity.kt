eb36dcec6ab2a7db83308b8f79f1dd79cf55e4e6
Erick Sanchez <erick.sanchez.p05@gmail.com>
2022-11-29 12:16:29 -0600

    Add: Activity refresh

--- SDKAndroid/src/main/java/app/cybrid/sdkandroid/BankAccountsActivity.kt
+++ SDKAndroid/src/main/java/app/cybrid/sdkandroid/BankAccountsActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import app.cybrid.sdkandroid.components.BankAccountsView
-import app.cybrid.sdkandroid.components.bankAccount.BankAccountViewModel
+import app.cybrid.sdkandroid.components.bankAccounts.view.BankAccountsViewModel
 
 class BankAccountsActivity : AppCompatActivity() {
 
@@ -13,7 +13,7 @@ class BankAccountsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bank_accounts)
 
-        val bankAccountViewModel: BankAccountViewModel by viewModels()
+        val bankAccountViewModel: BankAccountsViewModel by viewModels()
         val bankAccountView = findViewById<BankAccountsView>(R.id.bankAccountView)
 
         bankAccountView.setViewModel(bankAccountViewModel)
