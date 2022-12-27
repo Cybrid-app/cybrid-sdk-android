2ee7698ae673a5ddc3baec49275844b5ccfa6aa7
Erick Sanchez <erick.sanchez.p05@gmail.com>
2022-12-27 00:51:05 -0600

    Refactor TradeFlow e2e

--- app/src/androidTest/java/app/cybrid/demoapp/ui/tradeFlow/TradeFlowActivityTest.kt
+++ app/src/androidTest/java/app/cybrid/demoapp/ui/tradeFlow/TradeFlowActivityTest.kt
@@ -4,7 +4,9 @@ import android.content.res.Resources
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -12,6 +14,8 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import app.cybrid.demoapp.R
 import app.cybrid.demoapp.ui.login.LoginActivity
+import app.cybrid.demoapp.ui.util.waitUntilExists
+import app.cybrid.demoapp.ui.util.waitUntilViewIsDisplayed
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -40,30 +44,32 @@ class TradeFlowActivityTest {
     @Test
     fun checkAppFlow() = runTest {
 
-        Thread.sleep(2500)
-        onView(withId(R.id.demo)).check(matches(isDisplayed())).perform(click())
+        waitUntilViewIsDisplayed(withId(R.id.demo))
+        onView(withId(R.id.demo))
+            .check(matches(isDisplayed())).perform(click())
 
-        Thread.sleep(5000)
+        waitUntilViewIsDisplayed(withId(R.id.list))
         onView(withId(R.id.list)).check(matches(isDisplayed()))
-        onView(withText("TradeFlow")).check(matches(isDisplayed())).perform(click())
+        onView(withText("TradeFlow")).check(matches(isDisplayed())).perform(
+            click()
+        )
 
-        Thread.sleep(10000)
         compose.waitForIdle()
+
+        compose.waitUntilExists(hasTestTag("ListPricesView"), 10_000L)
         compose.onNodeWithTag("ListPricesView").assertIsDisplayed()
         compose.onNodeWithText("Search").assertIsDisplayed()
-        compose.onNodeWithText("Bitcoin").assertIsDisplayed()
-        compose.onNodeWithText("Ethereum").assertIsDisplayed()
+        compose.onNodeWithText("Bitcoin").assertExists()
+        compose.onNodeWithText("Ethereum").assertExists()
 
         compose.onNodeWithText("Search").performTextInput("Eth")
         compose.onNodeWithText("Bitcoin").assertDoesNotExist()
         compose.onNodeWithText("Ethereum").assertIsDisplayed()
-        Thread.sleep(5000)
 
+        Thread.sleep(3500)
         compose.onNodeWithText("Eth").performTextInput("")
         compose.onNodeWithText("Bitcoin").assertIsDisplayed()
         compose.onNodeWithText("Ethereum").assertIsDisplayed()
         compose.onNodeWithText("Bitcoin").performClick()
-
-        Thread.sleep(3000)
     }
 }
