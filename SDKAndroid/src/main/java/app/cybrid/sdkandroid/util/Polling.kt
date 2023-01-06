5be00d35628fb7352e1077db3413ea2909cd1f2f
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-01-06 10:27:48 -0600

    Remove nested functions inside Polling class

--- SDKAndroid/src/main/java/app/cybrid/sdkandroid/util/Polling.kt
+++ SDKAndroid/src/main/java/app/cybrid/sdkandroid/util/Polling.kt
@@ -24,22 +24,10 @@ class Polling(runner: () -> Unit) {
         this.executor.scheduleWithFixedDelay(this.runnable, updateInterval, updateInterval, TimeUnit.SECONDS)
     }
 
-    fun resume() {
-
-        this.runnable = Runnable { runner.invoke() }
-        this.executor = ScheduledThreadPoolExecutor(1)
-        this.executor.scheduleWithFixedDelay(this.runnable, updateInterval, updateInterval, TimeUnit.SECONDS)
-        this.canceled = false
-    }
-
     fun stop() {
 
         this.executor.shutdownNow()
         this.runnable = null
         this.canceled = true
-    }
-
-    fun isCanceled(): Boolean {
-        return this.canceled
     }
 }
