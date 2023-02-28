8e47038eb1dc8df252848dcaff92aa91a64c3103
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-02-28 10:43:17 -0600

    Fix e2e

--- app/src/main/java/app/cybrid/demoapp/core/App.kt
+++ app/src/main/java/app/cybrid/demoapp/core/App.kt
@@ -93,7 +93,7 @@ class App : Application(), CybridSDKEvents {
     companion object {
 
         private val demonEnv = CybridEnv.SANDBOX
-        private val demoUrl = "https://id.${demonEnv.name.lowercase()}.cybrid.app"
+        val demoUrl = "https://id.${demonEnv.name.lowercase()}.cybrid.app"
         const val TAG = "CybridSDKDemo"
 
         @SuppressLint("StaticFieldLeak")
